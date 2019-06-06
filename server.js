fbda9a13ae9071bad7f3d6898d6913e54b06b2ef
Petre Popescu <petre.popescu@microsoft.com>
2019-06-06 16:05:17 +0300

    First Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/hello/:name', (req, res) => {
+    res.send(`Hello, ${req.params.name}`)
+})
+
+app.listen(PORT, function () {
+    console.log(`App listening on port ${PORT}`)
+})
